d5e7f48e63a7c7b5119dce97915e9f9bda4da059
Семён <svlasov115@vk.com>
2024-02-10 23:30:14 +0500

    Added task 1 solving

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
 
+        int myMonthlySavings = 15000;
+        int myTotalOfAnnualSavings = 0;
+        int monthOfSavings = 0;
+        double percent = 1 / 100D;
+        double sum = 0;
+        int purpose = 2459000;
+
+        while (sum <= purpose) {
+            monthOfSavings++;
+            sum += sum * percent;
+            sum += myMonthlySavings;
+            System.out.println("Месяц " + monthOfSavings + "，сумма накоплений равна " + sum + " рублей");
+        }
     }
 }
